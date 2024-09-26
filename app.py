f557289b2b4d75b617446ceb3e12a3a7ecefe6a5
ateeb <mateeb.ce41ceme@ce.ceme.edu.pk>
2024-09-26 14:06:24 +0500

    Added app.py ngrox-2

--- app.py
+++ app.py
@@ -402,21 +402,21 @@ def start_tryon(dict, garm_img,garment_des,is_checked,is_checked_crop,use_ground
         return images[0], mask_gray
     # return images[0], mask_gray
 
-# garm_list = os.listdir(os.path.join(example_path,"cloth"))
-# garm_list_path = [os.path.join(example_path,"cloth",garm) for garm in garm_list]
+garm_list = os.listdir(os.path.join(example_path,"cloth"))
+garm_list_path = [os.path.join(example_path,"cloth",garm) for garm in garm_list]
 
-# human_list = os.listdir(os.path.join(example_path,"human"))
-# human_list_path = [os.path.join(example_path,"human",human) for human in human_list]
+human_list = os.listdir(os.path.join(example_path,"human"))
+human_list_path = [os.path.join(example_path,"human",human) for human in human_list]
 
-# human_ex_list = []
-# for ex_human in human_list_path:
-#     ex_dict= {}
-#     ex_dict['background'] = ex_human
-#     ex_dict['layers'] = None
-#     ex_dict['composite'] = None
-#     human_ex_list.append(ex_dict)
+human_ex_list = []
+for ex_human in human_list_path:
+    ex_dict= {}
+    ex_dict['background'] = ex_human
+    ex_dict['layers'] = None
+    ex_dict['composite'] = None
+    human_ex_list.append(ex_dict)
 
-# ##default human
+##default human
 
 
 image_blocks = gr.Blocks().queue()
@@ -435,21 +435,21 @@ with image_blocks as demo:
             with gr.Row():
                 has_hat = gr.Checkbox(label = 'Yes', info = 'Look for a hat to mask in the outfit', value = False)
                 has_gloves = gr.Checkbox(label = 'Yes', info = 'Look for a gloves to mask in the outfit', value = False)
-            # example = gr.Examples(
-            #     inputs=imgs,
-            #     examples_per_page=10,
-            #     examples=human_ex_list
-            # )
+            example = gr.Examples(
+                inputs=imgs,
+                examples_per_page=10,
+                examples=human_ex_list
+            )
 
         with gr.Column():
             garm_img = gr.Image(label="Garment", sources='upload', type="pil")
             with gr.Row(elem_id="prompt-container"):
                 with gr.Row():
                     prompt = gr.Textbox(placeholder="Description of garment ex) Short Sleeve Round Neck T-shirts", show_label=False, elem_id="prompt")
-            # example = gr.Examples(
-            #     inputs=garm_img,
-            #     examples_per_page=8,
-            #     examples=garm_list_path)
+            example = gr.Examples(
+                inputs=garm_img,
+                examples_per_page=8,
+                examples=garm_list_path)
         with gr.Column():
             # image_out = gr.Image(label="Output", elem_id="output-img", height=400)
             masked_img = gr.Image(label="Masked image output", elem_id="masked-img",show_share_button=False)
